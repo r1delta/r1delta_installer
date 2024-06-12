18bb76bfc06121e96b5ebca7d8d71d1373391a50
quad-damage <quadruple.damage@protonmail.com>
2024-06-12 20:28:58 +0300

    Turn off a few Love2D modules, remove OpenAL32.dll from final exe.

--- source/conf.lua
+++ source/conf.lua
@@ -5,4 +5,23 @@ function love.conf(t)
     t.window.width = 414
     t.window.height = 171
     t.console = false
+    
+    t.modules.audio = false 
+    t.modules.data = true   
+    t.modules.event = true  
+    t.modules.font = true   
+    t.modules.graphics = true   
+    t.modules.image = true  
+    t.modules.joystick = false  
+    t.modules.keyboard = true   
+    t.modules.math = true   
+    t.modules.mouse = true  
+    t.modules.physics = false   
+    t.modules.sound = false 
+    t.modules.system = true 
+    t.modules.thread = true 
+    t.modules.timer = true  
+    t.modules.touch = false 
+    t.modules.video = true  
+    t.modules.window = true 
 end
